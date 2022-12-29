9b4cdcc3bc6bb159416cf4d28a105f0c83635311
JCPrice0024 <jcprice0024@gmail.com>
2022-12-29 15:43:25 -0500

    First commit

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import lictestrepo3 "github.com/JCPrice0024/lic-testing/lic-testRepo3"
+
+func main() {
+	lictestrepo3.Subtraction(10, 5)
+}
---
+++ goodbye.go
@@ -0,0 +1,7 @@
+package lictestrepo5
+
+import "fmt"
+
+func GoodbyeUser(username string) string {
+	return fmt.Sprintf("Have a great night %s!", username)
+}
