2f61d1eab1bc5d773c34e2ac1516fcac3fd4ef6a
JCPrice0024 <jcprice0024@gmail.com>
2022-12-29 16:32:55 -0500

    Ready to Test

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	lictestrepo1 "github.com/JCPrice0024/lic-testRepo1"
+	lictestrepo2 "github.com/JCPrice0024/lic-testRepo2"
 	lictestrepo3 "github.com/JCPrice0024/lic-testRepo3"
+	lictestrepo4 "github.com/JCPrice0024/lic-testRepo4"
 )
 
 func main() {
 	fmt.Println(lictestrepo1.HelloUser("JCPrice0024"))
+	fmt.Println(lictestrepo2.WelcomeUser("JCPrice0024"))
 	fmt.Println(lictestrepo3.Subtraction(7127, 123))
+	fmt.Println(lictestrepo4.Addition(7127, 123))
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.19
 
 require (
 	github.com/JCPrice0024/lic-testRepo1 v0.0.0-20221229205625-2c7f453ff38b
+	github.com/JCPrice0024/lic-testRepo2 v0.0.0-20221229205555-8d8869287443
 	github.com/JCPrice0024/lic-testRepo3 v0.0.0-20221229205528-f34bd2e42a43
+	github.com/JCPrice0024/lic-testRepo4 v0.0.0-20221229205507-8cae7c274bcd
 )
